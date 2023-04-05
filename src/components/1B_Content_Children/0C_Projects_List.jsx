a475451a4d0d7744f49669d6c39d7140f421e7f1
unknown <jasonweerawardena@gmail.com>
2023-04-05 16:06:50 +0100

    project card information work in progress

--- src/components/1B_Content_Children/0C_Projects_List.jsx
+++ src/components/1B_Content_Children/0C_Projects_List.jsx
@@ -1,14 +1,16 @@
 import { SingleProjectCard } from "./1(0C child)_Single_Project_Card"
 import test_img from "../../Assests/test_img.jpg"
+import nc_news_fe from "../../Assests/NC_News_Front_End.png"
+import nc_news_logo from "../../Assests/NC_News_Logo.png"
 
 export const ProjectsList = () =>
 {
     const projectObject =
     [
-        {project_id : 0, project_name : 'test', image_1 : test_img, image_1_alt : 'test_image', image_2 : '', image_2_alt : 'test', short_description : 'test_short', long_description : 'teeeeeeessttt_loooongg'},
-        {project_id : 1, project_name : 'Nc_News', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : ''},
-        {project_id : 2, project_name : 'A-bit', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : ''},
-        {project_id : 3, project_name : 'C-Testing-Playground', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : ''},
+        {project_id : 0, project_name : 'test', image_0 : test_img, image_0_alt : 'test_image', image_1 : '', image_1_alt : 'test_image', image_2 : '', image_2_alt : 'test', short_description : 'test_short', long_description : 'teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeessttt_looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongg'},
+        {project_id : 1, project_name : 'Nc_News', image_0 : nc_news_logo, image_0_alt : '', image_1 : nc_news_fe, image_1_alt : '', image_2 : '', image_2_alt : '', short_description : 'Nc_News was my first full-stack project at Northcoders', long_description : ''},
+        {project_id : 2, project_name : 'A-bit', image_0 : '', image_0_alt : '', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : ''},
+        {project_id : 3, project_name : 'C-Testing-Playground', image_0 : '', image_0_alt : '', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : ''},
     ]
     return(
         <div className="Content_Child">
--- src/components/1B_Content_Children/1(0C child)_Single_Project_Card.jsx
+++ src/components/1B_Content_Children/1(0C child)_Single_Project_Card.jsx
@@ -5,22 +5,38 @@ export const SingleProjectCard = (props) =>
     const project = props.project
     /*add button in here that expands the page to display the full content
     information of the project */
-    const [expandedBool, setExpandedBool] = useState(false)
+    const [expandedBool, setExpandedBool] = useState(false);
+    function expandCard(event)
+    {
+        event.preventDefault();
+        setExpandedBool(true);
+    }
+    function collapseCard(event)
+    {
+        event.preventDefault();
+        setExpandedBool(false);
+    }
     return(
         expandedBool?
         <div className="Single_Project_Card">
             <h3 id="Single_Project_Header"> {project.project_name} </h3>
-            <div>
-                <img src={project.image_1} alt={project.image_1_alt} className="Single_Project_Image"/>
-                <div>{project.long_description}</div>
+            <div className="Single_Project_Card_Body">
+                <img src={project.image_0} alt={project.image_1_alt} className="Single_Project_Image"/>
+                <div className="Single_Project_Card_Information">
+                    <div>{project.long_description}</div>
+                    <div onClick={(event) => {collapseCard(event)}} className="Single_Project_Card_Expand_Collapse_Button">Read Less</div>
+                </div>
             </div>
         </div>
         :
         <div className="Single_Project_Card">
             <h3 id="Single_Project_Header"> {project.project_name} </h3>
-            <div>
-                <img src={project.image_1} alt={project.image_1_alt} className="Single_Project_Image"/>
-                <div>{project.short_description}</div>
+            <div className="Single_Project_Card_Body">
+                <img src={project.image_0} alt={project.image_1_alt} className="Single_Project_Image"/>
+                <div className="Single_Project_Card_Information">
+                    <div>{project.short_description}</div>
+                    <div onClick={(event) => {expandCard(event)}} className="Single_Project_Card_Expand_Collapse_Button">Read Less</div>
+                </div>
             </div>
         </div>
     )
