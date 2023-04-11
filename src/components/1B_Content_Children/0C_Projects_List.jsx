6c85e41d61768d41e048dac30518797e9408fd5a
unknown <jasonweerawardena@gmail.com>
2023-04-11 17:06:41 +0100

    bug with desc render remains

--- src/components/1B_Content_Children/0C_Projects_List.jsx
+++ src/components/1B_Content_Children/0C_Projects_List.jsx
@@ -1,5 +1,7 @@
 import { SingleProjectCard } from "./1(0C child)_Single_Project_Card"
-import test_img from "../../Assests/test_img.jpg"
+import test_img from "../../Assests/test_img_logo.jpg"
+import test_img_1 from "../../Assests/test_img_1.jpg"
+import test_img_2 from "../../Assests/test_img_2.jpg"
 import nc_news_fe from "../../Assests/NC_News_Front_End.png"
 import nc_news_logo from "../../Assests/NC_News_Logo.png"
 
@@ -7,10 +9,17 @@ export const ProjectsList = () =>
 {
     const projectObject =
     [
-        {project_id : 0, project_name : 'test', image_0 : test_img, image_0_alt : 'test_image', image_1 : '', image_1_alt : 'test_image', image_2 : '', image_2_alt : 'test', short_description : 'test_short', long_description : 'teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeessttt_looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongg'},
-        {project_id : 1, project_name : 'Nc_News', image_0 : nc_news_logo, image_0_alt : '', image_1 : nc_news_fe, image_1_alt : '', image_2 : '', image_2_alt : '', short_description : 'Nc_News was my first full-stack project at Northcoders using a PSQL express backend framework hosted on render and a react.js frontend hosted on netlify', long_description : ''},
-        {project_id : 2, project_name : 'A-bit', image_0 : '', image_0_alt : '', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : ''},
-        {project_id : 3, project_name : 'C-Testing-Playground', image_0 : '', image_0_alt : '', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : ''},
+        {project_id : 0, project_name : 'test', image_0 : test_img, image_0_alt : 'test_image', image_1 : test_img_1, image_1_alt : 'test_image', image_2 : test_img_2, image_2_alt : 'test', short_description : 'test_short', long_description : `
+        Morbi ultricies porta ex at aliquet. Suspendisse mollis lacus ac neque imperdiet sagittis. Sed eu ipsum eget lorem placerat ullamcorper. Ut iaculis arcu nec mauris eleifend condimentum. Vivamus tincidunt augue nec tortor laoreet, quis mollis tortor consequat. In nec odio pellentesque leo molestie congue. In vestibulum luctus semper. Mauris maximus accumsan purus in vestibulum. Pellentesque non dolor sollicitudin, tempus purus nec, tincidunt mi. Phasellus neque lectus, efficitur et porttitor ac, consectetur vehicula nibh. Pellentesque faucibus, lorem id placerat egestas, est libero tristique arcu, in mattis mauris turpis a erat. Proin vel ex ut augue iaculis feugiat. Maecenas in lectus sem. Vivamus sollicitudin facilisis velit eu auctor. Nunc eget diam tempus, consectetur orci at, tincidunt ligula.
+        
+        Etiam egestas suscipit sem, pharetra suscipit purus malesuada eget. Integer tortor dolor, luctus ac fringilla et, interdum nec orci. Sed ultrices odio nec enim malesuada, at cursus velit laoreet. Maecenas eu mauris in ligula pretium viverra non nec nunc. Maecenas blandit iaculis mattis. Ut arcu quam, venenatis pellentesque lorem vel, vulputate accumsan diam. Vivamus eget purus scelerisque, vehicula lectus non, dapibus arcu. Donec nec quam vitae eros sagittis sollicitudin ut ac lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam suscipit, lacus et aliquet faucibus, nulla elit viverra nibh, vel pulvinar mi odio sit amet orci. Nulla metus ligula, laoreet eget dui vel, ultricies sollicitudin ante. Maecenas condimentum volutpat nisi, vitae scelerisque est vestibulum in. Vivamus a quam at quam ornare imperdiet. Pellentesque tempus, dolor at sollicitudin bibendum, odio mi luctus ligula, a vulputate massa justo in tortor. Cras tellus nisi, condimentum non elit ac, laoreet vulputate tortor.
+        
+        Nulla ac purus augue. Mauris a hendrerit arcu. Curabitur nulla tellus, vehicula ac fringilla eget, vehicula sit amet ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec nec nulla id est suscipit mollis in et tortor. Suspendisse in interdum augue. Fusce nibh diam, luctus a semper eu, sagittis id nulla. Aliquam non leo sit amet mauris faucibus efficitur non vitae purus. Aliquam at pretium nisi, nec ullamcorper sapien. Sed congue mauris ac justo pharetra, id convallis lectus pellentesque. Curabitur pellentesque erat et massa dapibus malesuada.
+        
+        Suspendisse mollis, elit vitae malesuada tempus, mi risus tincidunt nisi, eget malesuada diam justo id ante. Maecenas tristique, eros ac egestas iaculis, augue dui porta leo, vitae fringilla lorem est a sapien. Suspendisse potenti. Pellentesque egestas sollicitudin mi in semper. Quisque tempor laoreet.`},
+        {project_id : 1, project_name : 'Nc_News', image_0 : nc_news_logo, image_0_alt : '', image_1 : nc_news_fe, image_1_alt : '', image_2 : '', image_2_alt : '', short_description : 'Nc_News was my first full-stack project at Northcoders using a PSQL express backend framework hosted on render and a react.js frontend hosted on netlify', long_description : `not this one`},
+        {project_id : 2, project_name : 'A-bit', image_0 : '', image_0_alt : '', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : `not this one`},
+        {project_id : 3, project_name : 'C-Testing-Playground', image_0 : '', image_0_alt : '', image_1 : '', image_1_alt : '', image_2 : '', image_2_alt : '', short_description : '', long_description : `not this one`},
     ]
     return(
         <div className="Content_Child">
--- src/components/1B_Content_Children/1(0C child)_Single_Project_Card.jsx
+++ src/components/1B_Content_Children/1(0C child)_Single_Project_Card.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import { useEffect, useState, useRef } from "react"
 
 export const SingleProjectCard = (props) =>
 {
     const project = props.project
-    /*add button in here that expands the page to display the full content
-    information of the project */
+    
+    let projectDescription1 = useRef("starter_string_fe");
+    let projectDescription2 = useRef("starter_string_be");
+
     const [expandedBool, setExpandedBool] = useState(false);
     function expandCard(event)
     {
@@ -16,13 +18,36 @@ export const SingleProjectCard = (props) =>
         event.preventDefault();
         setExpandedBool(false);
     }
+
+    useEffect(() => 
+    {
+        if(project.long_description.length !== 0)
+        {
+            console.log(project.long_description)
+            for(let i = (project.long_description.length / 2); i < project.long_description.length; i++)
+            {
+                if(project.long_description[i] === " ")
+                {
+                    projectDescription1.current = project.long_description.slice(0, i);
+                    projectDescription2.current = project.long_description.slice(i)
+                }
+            }
+        }else
+        {
+            console.log("error: empty string")
+        }
+    }, [expandedBool]);
+
     return(
         expandedBool?
         <div className="Single_Project_Card_Expanded">
             <h3 id="Single_Project_Header"> {project.project_name} </h3>
             <div className="Single_Project_Card_Body">
                 <div className="Single_Project_Card_Information_Expanded">
-                    <div>{project.long_description}</div>
+                    <div>{projectDescription1}</div>
+                    <img src={project.image_1} alt="" className="Single_Project_Image"/>
+                    <div>{projectDescription2}</div>
+                    <img src={project.image_2} alt="" className="Single_Project_Image"/>
                     <div onClick={(event) => {collapseCard(event)}} className="Single_Project_Card_Expand_Collapse_Button">Read Less</div>
                 </div>
             </div>
@@ -31,10 +56,10 @@ export const SingleProjectCard = (props) =>
         <div className="Single_Project_Card_Shortened">
             <h3 id="Single_Project_Header"> {project.project_name} </h3>
             <div className="Single_Project_Card_Body">
-                <img src={project.image_0} alt={project.image_1_alt} className="Single_Project_Image"/>
+                <img src={project.image_0} alt={project.image_1_alt} className="Single_Project_Image_Logo"/>
                 <div className="Single_Project_Card_Information_Shortened">
                     <div>{project.short_description}</div>
-                    <div onClick={(event) => {expandCard(event)}} className="Single_Project_Card_Expand_Collapse_Button">Read Less</div>
+                    <div onClick={(event) => {expandCard(event)}} className="Single_Project_Card_Expand_Collapse_Button">Read More</div>
                 </div>
             </div>
         </div>
